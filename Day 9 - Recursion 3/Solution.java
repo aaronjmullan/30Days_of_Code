4f3dce4b877cf85de0ab5adc05f422e8d1b0de38
aaronjmullan <aaronjmullan@gmail.com>
2021-11-21 19:07:24 +0000

    Create Solution.java

---
+++ Day 9 - Recursion 3/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        Scanner readIn = new Scanner(System.in);
+        int n = readIn.nextInt();
+        readIn.close();
+        System.out.println(factorial(n));
+        
+    }
+
+    private static int factorial(int n) {
+        
+        if (n == 1) return 1;
+        return factorial(n - 1) * n;
+        
+    }
+}
